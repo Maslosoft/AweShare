b40c84c64fc5840d9ebd928ac126b09f1989fffa
Peter Maselkowski <pmaselkowski@gmail.com>
2015-11-09 17:45:31 +0100

    * dev

--- dev/index.php
+++ dev/index.php
@@ -13,6 +13,9 @@ Brand background
 <div class="awe-share awe-share-brand-bg"></div>
 Vertical
 <div class="awe-share awe-share-vertical"></div>
+
+Vertical brand background
+<div class="awe-share awe-share-vertical awe-share-brand-bg"></div>
 </p>
 
 <div id="log">
