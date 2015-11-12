b5e43667e9e70e10111bd25e0b58574dbe2a5c56
Peter Maselkowski <pmaselkowski@gmail.com>
2015-11-12 09:50:24 +0100

    + Support for collapse empty counters * Ensure icons without counters are square

--- dev/index.php
+++ dev/index.php
@@ -10,12 +10,12 @@ require '_header.php'
 </div>
 <div class="row">
 	<div class="col-md-12">
-		<h4>Horizontal (http://stackoverflow.com/) empty counter placeholder</h4>
+		<h4>Horizontal (http://stackoverflow.com/) and empty counter placeholder</h4>
 		<div class="awe-share" data-url="http://stackoverflow.com/" data-counter-empty="&#9787;"></div>
 		<h4>Selected services and no counter</h4>
 		<div class="awe-share" data-counter="false" data-services=" facebook,  twitter, google-plus ,github  "></div>
-		<h4>Brand background (http://google.com/)</h4>
-		<div class="awe-share awe-share-brand-bg" data-url="http://google.com/"></div>
+		<h4>Brand background (http://google.com/) and collapse empty</h4>
+		<div class="awe-share awe-share-brand-bg awe-share-collapse-empty" data-url="http://google.com/"></div>
 		<h4>Brand background on hover</h4>
 		<div class="awe-share awe-share-brand-bg-hover"></div>
 		<h4>Brand background with hover foreground (http://deviantart.com/)</h4>
@@ -27,20 +27,36 @@ require '_header.php'
 		<div class="awe-share awe-share-brand-fg-hover"></div>
 		<h4>Brand foreground with hover background (http://facebook.com/)</h4>
 		<div class="awe-share awe-share-brand-fg awe-share-brand-bg-hover" data-url="http://facebook.com/"></div>
+		<h4>Brand foreground with hover background (http://facebook.com/) smaller</h4>
+		<div class="awe-share awe-share-brand-fg awe-share-brand-bg-hover" style="font-size: .8em;" data-url="http://facebook.com/"></div>
 	</div>
 </div>
 <div class="row">
 	<div class="col-sm-2">
 		<h4>Vertical and no counter</h4>
-		<div class="awe-share awe-share-vertical" data-counter="false"></div>
 	</div>
 	<div class="col-sm-2">
 		<h4>Vertical brand background and no counter</h4>
-		<div class="awe-share awe-share-vertical awe-share-brand-bg" data-counter="false"></div>
 	</div>
 	<div class="col-sm-2">
 		<h4>Vertical brand background (http://facebook.com/)</h4>
+	</div>
+	<div class="col-sm-2">
+		<h4>Vertical brand background (http://facebook.com/) and collapse empty</h4>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-2">
+		<div class="awe-share awe-share-vertical" data-counter="false"></div>
+	</div>
+	<div class="col-sm-2">
+		<div class="awe-share awe-share-vertical awe-share-brand-bg" data-counter="false"></div>
+	</div>
+	<div class="col-sm-2">
 		<div class="awe-share awe-share-vertical awe-share-brand-bg" data-url="http://facebook.com/"></div>
+	</div>
+	<div class="col-sm-2">
+		<div class="awe-share awe-share-vertical awe-share-brand-bg awe-share-collapse-empty" data-url="http://facebook.com/"></div>
 	</div>
 </div>
 </p>
